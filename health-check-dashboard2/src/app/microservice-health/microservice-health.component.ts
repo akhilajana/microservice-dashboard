239aa518ebcef4173da6516153d029d311329d12
Akhila Jana <32227881+akhilajana@users.noreply.github.com>
2024-11-20 04:40:37 -0600

    Clean up charts

--- health-check-dashboard2/src/app/microservice-health/microservice-health.component.ts
+++ health-check-dashboard2/src/app/microservice-health/microservice-health.component.ts
@@ -10,6 +10,7 @@ import { HealthData, MicroserviceData } from '../interface/microservice-interfac
 })
 export class MicroserviceHealthComponent implements OnInit {
   @Input() healthData: any = {};
+  healthData1: any;
 
   constructor(private healthCheckService: HealthCheckService) { }
 
@@ -33,11 +34,37 @@ export class MicroserviceHealthComponent implements OnInit {
             }
           })).filter(item => item.environment != "dependentServices")
         }));
-        console.log(this.healthData);
+        console.log("Filtered"+this.healthData);
       },
       error => {
         console.error('Error fetching health data:', error);
       }
     );
+
+    this.healthCheckService.getHealthCheckData().subscribe(
+      data => {
+        this.healthData1 = data;
+        this.healthData1 = Object.entries(data).map(([microService, data]: [string, any]) => ({
+          microServiceName: microService,
+          data: Object.entries(data).map(([env, data]: [string, any]) => ({
+            environment: env,
+            data: {
+              branchName: data.branchName,
+              pods: Object.entries(data)
+              .map(([podName, data]: [string, any]) => ({
+                podName: podName,
+                status: data.status,
+                systemUsage: data.systemUsage,
+                memory: data.memory,
+              }))
+            }
+          })).filter(item => item.environment == "dependentServices")
+        }));
+        console.log(this.healthData1);
+      },
+      error => {
+        console.error('Error fetching health data 222:', error);
+      }
+    )
   }
 }
--- health-check-dashboard2/src/app/pod-health/pod-health.component.ts
+++ health-check-dashboard2/src/app/pod-health/pod-health.component.ts
@@ -24,8 +24,10 @@ export class PodHealthComponent implements OnInit {
   chartData: any[] = [];
   memoryUsageCanvasId: any;
   systemUsageCanvasId: any;
+  healthCanvasId: any;
 
   ngOnInit() {
+    this.healthCanvasId = this.microservice.microServiceName +this.env?.environment + 'health';
     this.memoryUsageCanvasId = this.microservice.microServiceName +this.env?.environment + 'memory';
     this.systemUsageCanvasId = this.microservice.microServiceName +this.env?.environment + 'systemUsage';
   }
@@ -35,9 +37,29 @@ export class PodHealthComponent implements OnInit {
   }
 
   updateChartData() {
-    this.initializeBarChart(this.memoryUsageCanvasId, this.pods.map(pod => pod.podName),this.pods.map(pod => pod.memory), 'Memory Usage for ' + this.env.environment );
-
-    this.initializeBarChart(this.systemUsageCanvasId, this.pods.map(pod => pod.podName),this.pods.map(pod => pod.systemUsage), 'System Usage for ' + this.env.environment );
+    const podStatuses = this.pods.map( pod => pod.status === 'UP'? 100 :10);
+    this.initializePieChart(this.healthCanvasId, this.pods.map(pod => pod.podName),podStatuses, 'Health' );
+    this.initializeBarChart(this.memoryUsageCanvasId, this.pods.map(pod => pod.podName),this.pods.map(pod => pod.memory), 'Memory Usage' );
+    this.initializeBarChart(this.systemUsageCanvasId, this.pods.map(pod => pod.podName),this.pods.map(pod => pod.systemUsage), 'System Usage' );
+  }
+  initializePieChart(canvasid: any, labels: any, values:any, title: any) {
+    const pieChartElement = document.getElementById(canvasid) as HTMLCanvasElement;
+    return new Chart(pieChartElement, {
+      type: ChartType.PIE,
+      data: {
+        labels: labels,
+        datasets: [{data: values,
+        backgroundColor: ['rgb(40, 167, 69)','rgb(0,123,255)','rgb(253,126,20)','rgb(220,53,69)'],
+        borderColor: ['rgb(40, 167, 69)','rgb(0,123,255)','rgb(253,126,20)','rgb(220,53,69)'],
+        borderWidth: 3
+      }]},
+      options: {
+        plugins: {
+          title: { display: true, text: [title]},
+          legend: {display: false}
+        }
+      } 
+    })
   }
 
 
